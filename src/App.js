3e7df413ee953cc583441957cd48413bc34071dd
claridiva2000 <claridiva2000@gmail.com>
2019-04-25 00:55:39 -0500

    trying to get this initial load to cooperate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
 
-import Login from './components/Login'
-import Main from './components/Main'
-import AddNew from './components/AddNew'
+import Login from './components/Login';
+import Main from './components/Main';
+import AddNew from './components/AddNew';
 
 function App() {
   return (
     <div className="App">
-    <Login />
-    <Main/>
-    <AddNew/>
+      <Login />
+      <Main />
+      <AddNew />
     </div>
   );
 }
-
 
 export default App;
